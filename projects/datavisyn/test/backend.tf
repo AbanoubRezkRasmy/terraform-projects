d8b1f4bc20e7e653036195dd38564af39586f99b
Abanoub Rizk <abanoub.rizk@vodafone.com>
2025-05-28 20:28:34 +0300

    change repo

--- projects/datavisyn/test/backend.tf
+++ projects/datavisyn/test/backend.tf
@@ -11,8 +11,8 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "platform-test-remote-tfstate" 
-    key            = "platform/test/platform-test"   
+    bucket         = "terrform-datavisyn-tfstate" 
+    key            = "datavisyn/test/datavisyn-test"   
     region         = "eu-west-1"                   
     encrypt        = true
     use_lockfile   = true
