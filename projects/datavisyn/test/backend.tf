297cf2be3d0a94df2af5e676c5a5c30ebdb1c1d2
Khaled <khaled.ahmed@btech.com>
2025-05-30 18:22:58 +0300

    Update backend.tf

--- projects/datavisyn/test/backend.tf
+++ projects/datavisyn/test/backend.tf
@@ -12,7 +12,7 @@ terraform {
   }
   backend "s3" {
     bucket         = "terrform-datavisyn-tfstate" 
-    key            = "datavisyn/test/datavisyn-test"   
+    key            = "datavisynn/test/datavisyn-test"   
     region         = "eu-west-1"                   
     #encrypt        = true
     #use_lockfile   = true
